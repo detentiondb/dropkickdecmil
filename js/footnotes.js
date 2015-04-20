938166e5307f60163cc123c43c12f3e485be8992
Detention Database <detentiondb@gmail.com>
2015-04-20 22:27:52 +0800

    added footnotes to About and Home; updated flyer and poster to versions excluding ACTU references

--- js/footnotes.js
+++ js/footnotes.js
@@ -1,8 +1,10 @@
 var section = $('.hd-footnote-section');
 var refno = 0; 
 $('.hd-footnote').each(function (i, fn) {
-	refno++; 
-	section.append('<li>' + fn.innerHTML + '</li>');
-	fn.innerHTML = '<a href="#fn' + refno + '">' + refno + '</a>';
+	refno++;
+	var id = 'fn' + refno; 
+	var reference = '<li id="' + id + '">' + fn.innerHTML + '</li>'; 
+	section.append(reference);
+	fn.innerHTML = '<a href="#' + id + '">' + refno + '</a>';
 });
 
