094cc3ddd522bb55745437785c8b85a67ee506e7
Detention Database <detentiondb@gmail.com>
2015-09-04 00:19:53 +1000

    Dropkick Decmil deploy test

--- project.clj
+++ project.clj
@@ -1,16 +1,25 @@
 (defproject f4f "0.1.0-SNAPSHOT"
   :source-paths ["."]
 
-  :dependencies [[org.clojure/clojure "1.6.0"]
+  :eval-in :leiningen
+
+  :dependencies [[org.clojure/clojure "1.7.0"]
                  [org.clojure/clojurescript "0.0-2371" :scope "provided"]
                  [org.clojure/core.async "0.1.346.0-17112a-alpha"]
                  [om "0.7.3"]
                  [pani "0.0.2"]
                  [sablono "0.2.22"]
                  [cljs-uuid "0.0.4"]
-                 [leiningen "2.5.0"]]
+                 [leiningen "2.5.0"]
+                 [org.clojure/data.json "0.2.6"]]
 
-  :plugins [[lein-cljsbuild "1.0.3"]]
+  :plugins [[lein-s3-static-deploy "0.1.0"]
+            [lein-cljsbuild "1.0.3"]]
+
+  :aws {:access-key ~(System/getenv "AWS_ACCESS_KEY") 
+        :secret-key ~(System/getenv "AWS_SECRET_KEY") 
+        :s3-static-deploy {:bucket "hestadivest.net" 
+                           :local-root "site"}}
 
   :min-lein-version "2.5.0"
 
